f45a8021d085c2a9796b2709125e3944246a31ff
Iamdachi04 <dachi4github@gmail.com>
2025-08-06 15:38:57 +0400

    created a new function that has to carry out the execution of changing the opacity and the filter of the image , and also the text of the percentage of the value that we have been given from the mapRange function.

--- main.js
+++ main.js
@@ -6,5 +6,12 @@ const loadingText= document.querySelector(".loading-text");
     function mapRange(value, in_min, in_max, out_min, out_max){
       return (value) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
-    
+    function updateVisuals(value){
+      const blur= mapRange(value , 0,100,20,0);
+      const opacity = mapRange(value,0,100,1,0);
+      image.style.filter = 'blur('+blur+'px';
+      loadingText.innerText= `${value.toFixed(1)}%`;
+      loadingText.style.opacity = opacity;
+    }
+
 })
